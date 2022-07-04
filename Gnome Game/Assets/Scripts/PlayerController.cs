0f141a76fede2488d0d7671e5ee4eb56eb27516f
remus.jones <remus.jones@hotmail.com>
2022-07-04 11:14:08 +1000

    Smoother camera

--- Gnome Game/Assets/Scripts/PlayerController.cs
+++ Gnome Game/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour, GnomeGameActions.IPlayerActions
 {
-    public CharacterController controller;
-    public Vector3 movement;
-    public Vector3 lastMovement;
-
-    public Vector3 cyanBallPos;
-    public Vector3 offset = Vector3.up;
-    public float maxOffset;
-    public float offsetSpeed;
-    public float cameraSpeed;
-    public float rotationSpeed = 1000;
+   
+   
+    [ReadOnly,BoxGroup("Debug"), SerializeField] private Vector3 movement;
+    [ReadOnly,BoxGroup("Debug"), SerializeField] private Vector3 lastMovement;
+    [ReadOnly,BoxGroup("Debug"), SerializeField] Vector3 cyanBallPos;
+    [ReadOnly,BoxGroup("Debug"), SerializeField] private Vector3 offset = Vector3.zero;
+    
+    [Title("PlayerController Settings")]
+    [SerializeField] public CharacterController controller;
+    [SerializeField] private float maxOffset;
+    [SerializeField] private float offsetSpeed;
+    [SerializeField] private float cameraSpeed = 0.001f;
+    [SerializeField] private float playerSpeed = 3f;
+    [SerializeField] private float rotationSpeed = 1000;
     public CameraController CameraController;
-
     public WeaponBase activeWeapon;
 
 
+    
     public void Start()
     {
         Initialize();
@@ -45,7 +50,7 @@ public class PlayerController : MonoBehaviour, GnomeGameActions.IPlayerActions
     private void FixedUpdate()
     {
         Vector3 movementLastFrame = movement;
-        controller.Move(movement * Time.fixedDeltaTime * 3);
+        controller.Move(movement * (Time.deltaTime * playerSpeed));
 
         if (movement != Vector3.zero)
         {
